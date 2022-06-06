5679cc4eb7befa275c1da2e32584ec6056f80e36
Ridercogswell <ridercogswell@gmail.com>
2022-06-05 23:48:57 -0700

    add href

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Modal = ({ onClose, currentProject }) => {
-    const { title, technologies, description } = currentProject;
+    const { title, technologies, description, link } = currentProject;
 
     return(
         <div className="modalBackdrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">{title}</h3>
-                <img src={(`/assets/${title}/.png`)} alt="current project"/>
+                <a href={link} target="project">
+                  <img src={(`/assets/${title}/.png`)} alt="current project"/>
+                </a>
                 <p>
                   {description}
                   {technologies}
--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -7,40 +7,46 @@ const ProjectList = ({ title }) => {
 
     const projects = [
         {
+            title: 'due',
+            technologies: 'React, GraohQL, Mongoose, MongoDB, Express.js, Node.js',
+            description: 'Modern application to help everyone track their finances with a Non-Relational databsase and smart design.',
+            image: 'loople.png',
+            link: 'https://due-ucd.herokuapp.com/'
+        },
+        {
             title: 'cmnt',
             technologies: 'Node.js, Express.js, MySQL, Sequelize, Handlebars, Heroku',
             description: 'Cmnt is a Node.js application that empowers anonymous users with a random topic that they can comment on, like, share, etc.',
-            image: 'cmnt.png'
+            image: 'cmnt.png',
+            link: 'https://cmnt-app.herokuapp.com/'
         },
         {
             title: 'loople',
             technologies: 'Vanilla JS, HTML, CSS, Foundation, Youtube API, The News API',
             description: 'Loople is an interactive webpage that allows a user to search a topic and be given a response on the latest news, videos, and in the future, social media posts',
-            image: 'loople.png'
+            image: 'loople.png',
+            link: 'https://ridercogswell.github.io/tunedin/'
         },        
         {
             title: 'Deep Thoughts',
             technologies: 'GraphQL, React, Mongoose, MongoDB, Express.js, Node.js',
             description: 'Social network platform allowing users, to comment, friend, post and react!',
-            image: 'loople.png'
+            image: 'loople.png',
+            link: ''
         },
         {
             title: 'Food Festival',
             technologies: 'Node.js, WebPack, HTML, CSS, PWA',
             description: 'Website for local food festival where you can reserve tickets, check the schedule, and more!',
-            image: 'loople.png'
+            image: 'loople.png',
+            link: ''
         },
         {
-            title: 'Social Network API',
-            technologies: 'Mongoose, MongoDB, Express.js, Node.js',
-            description: 'Backend for a modern social network that utilizes a Non-Relational database.',
-            image: 'loople.png'
-        },
-        {
-            title: 'Budget Tracker',
-            technologies: 'Mongoose, MongoDB, IndexedDB, Express.js, Node.js',
-            description: 'Modern application to help everyone track their finances with a Non-Relational databsase and smart design.',
-            image: 'loople.png'
+            title: 'Shop-Shop',
+            technologies: 'React, GraphQL, PWA, Mongoose, MongoDB, Express.js, Node.js, Stripe',
+            description: 'E-Commerce Application utilizing modern technologies.',
+            image: 'loople.png',
+            link: ''
         }
     ];
 
@@ -63,7 +69,7 @@ const ProjectList = ({ title }) => {
             <div className='flex-row'>
                 {projects.map((project, i) => (
                     <img 
-                        src={`/assets/img/${project.image}`}
+                        src={`/assets/${project.image}`}
                         alt={project.title}
                         className='img-thumbnail mx-1'
                         onClick={() => toggleModal(project, i)}
